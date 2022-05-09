2257080bd48531144122e4eb81ed3ffbada1ecdb
Marcelo <marcelo.mfilho2804@gmail.com>
2022-05-09 11:33:07 -0300

    changes for e2e tets

---
+++ tests/unit/recommendationService.test.ts
@@ -0,0 +1,29 @@
+describe("recommendation service unit tests", () => {
+    it("should insert a recommendation", () => {
+        
+    })
+
+    it("should increase the number of votes", () => {
+        
+    })
+    
+    it("should decrease the number of votes", () => {
+        
+    })
+
+    it("should get a random recommendation", () => {
+        
+    })
+
+    it("shoukd get all Recommendations", () => {
+        
+    })
+
+    it("should get a recommendations by its is", () => {
+        
+    })
+
+    it("should get recommendations ordered by the number of votes", () => {
+        
+    })
+})
